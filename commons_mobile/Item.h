9f9d369ae2e1ffe900c3d9022fbf386cc65d2012
João Magalhães <joamag@hive.pt>
2011-03-14 11:14:06 +0000

    new items

---
+++ commons_mobile/Item.h
@@ -0,0 +1,16 @@
+//
+//  Item.h
+//  commons_mobile
+//
+//  Created by João Magalhães on 3/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Item : NSObject {
+    
+}
+
+@end
---
+++ commons_mobile/Item.m
@@ -0,0 +1,13 @@
+//
+//  Item.m
+//  commons_mobile
+//
+//  Created by João Magalhães on 3/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Item.h"
+
+@implementation Item
+
+@end
